b7102652f22451afc59249cef40516fc959bb83b
Rafael Muttoni <rafael@muttoni.com.br>
2023-04-29 06:29:13 -0300

    removed bg

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onInstalled.addListener((reason) => {
-  if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
-    console.log("Extension installed.");
-    chrome.tabs.create({ url: "tutorial.html" });
-  }
-});
